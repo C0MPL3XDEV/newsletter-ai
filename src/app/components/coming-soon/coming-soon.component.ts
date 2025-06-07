7fd6fb89c9eff55d40225846f3e8da4fc316d0fa
Carmine Dev <78054834+C0MPL3XDEV@users.noreply.github.com>
2025-06-07 18:58:53 +0200

    Add ngx-fade component and update coming soon section with animations

--- src/app/components/coming-soon/coming-soon.component.ts
+++ src/app/components/coming-soon/coming-soon.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {TopbarComponent} from '../topbar/topbar.component';
 import {NgxGradientTextComponent} from '@omnedia/ngx-gradient-text';
 import {RippleBackgroundComponent} from '../ripple-background/ripple-background.component';
+import {NgxFadeComponent} from '@omnedia/ngx-fade';
 
 @Component({
   selector: 'app-coming-soon',
@@ -9,6 +10,7 @@ import {RippleBackgroundComponent} from '../ripple-background/ripple-background.
     TopbarComponent,
     NgxGradientTextComponent,
     RippleBackgroundComponent,
+    NgxFadeComponent,
   ],
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.css'
