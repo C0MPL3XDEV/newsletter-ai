04906065f0ec38498964510a366bbe5e3a10e75d
Carmine Dev <78054834+C0MPL3XDEV@users.noreply.github.com>
2025-06-11 00:46:03 +0200

    Add API service and environment configurations; update coming soon component

--- src/app/components/coming-soon/coming-soon.component.ts
+++ src/app/components/coming-soon/coming-soon.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TopbarComponent} from '../topbar/topbar.component';
 import {NgxGradientTextComponent} from '@omnedia/ngx-gradient-text';
 import {RippleBackgroundComponent} from '../ripple-background/ripple-background.component';
 import {NgxFadeComponent} from '@omnedia/ngx-fade';
 import {ComingSoonFormComponent} from '../coming-soon-form/coming-soon-form.component';
+import {ApiService} from '../../api.service';
 
 @Component({
   selector: 'app-coming-soon',
@@ -17,7 +18,20 @@ import {ComingSoonFormComponent} from '../coming-soon-form/coming-soon-form.comp
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.css'
 })
-export class ComingSoonComponent {
+export class ComingSoonComponent implements OnInit {
 
   date = new Date();
+
+  constructor(private apiService: ApiService) {
+  }
+
+  testCall: any;
+
+  ngOnInit() {
+    this.apiService.testCall().subscribe({
+      next: (response) => {
+        this.testCall = response;
+      }
+    })
+  }
 }
