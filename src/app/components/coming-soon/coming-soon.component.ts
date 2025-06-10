71708dfa50a7ba55af1c144eac7445d0983fd7e2
Carmine Dev <78054834+C0MPL3XDEV@users.noreply.github.com>
2025-06-11 01:15:06 +0200

    Provide httpClient

--- src/app/components/coming-soon/coming-soon.component.ts
+++ src/app/components/coming-soon/coming-soon.component.ts
@@ -1,10 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {TopbarComponent} from '../topbar/topbar.component';
 import {NgxGradientTextComponent} from '@omnedia/ngx-gradient-text';
 import {RippleBackgroundComponent} from '../ripple-background/ripple-background.component';
 import {NgxFadeComponent} from '@omnedia/ngx-fade';
 import {ComingSoonFormComponent} from '../coming-soon-form/coming-soon-form.component';
-import {ApiService} from '../../api.service';
 
 @Component({
   selector: 'app-coming-soon',
@@ -18,20 +17,10 @@ import {ApiService} from '../../api.service';
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.css'
 })
-export class ComingSoonComponent implements OnInit {
+export class ComingSoonComponent {
 
   date = new Date();
 
-  constructor(private apiService: ApiService) {
-  }
-
-  testCallResponse: any;
-
-  ngOnInit() {
-    this.apiService.testCall().subscribe({
-      next: (response) => {
-        this.testCallResponse = response;
-      }
-    })
+  constructor() {
   }
 }
