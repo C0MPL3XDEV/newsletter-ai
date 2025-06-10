83f141bae5074aa8d33539510014ae5507856c8b
Carmine Dev <78054834+C0MPL3XDEV@users.noreply.github.com>
2025-06-11 00:53:02 +0200

    Provide httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,10 @@ import {routes} from './app.routes';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideToastr} from 'ngx-toastr';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimations(), provideToastr({
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideHttpClient(), provideClientHydration(withEventReplay()), provideAnimations(), provideToastr({
     positionClass: 'toast-top-right',
     timeOut: 4000,
     closeButton: true,
